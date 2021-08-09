0eb5e96b375c4bf2bbcbf7f6f934d9930c58599c
TO85 <88366272+TO85@users.noreply.github.com>
2021-08-09 23:46:44 +0000

    Update Random.cpp

--- src/apps/desktop/PlayTTT/Random.cpp
+++ src/apps/desktop/PlayTTT/Random.cpp
@@ -1,9 +1,26 @@
 #include "Random.h"
 
+void Random::ctor()
+{
+    (void)peek();
+}
+
+unsigned peek() const
+{
+    if (mCurrentChunk.isEmpty())
+        mCurrentChunk = takeBuffer();
+    return mCurrentChunk.first();
+}
+
+
 // private
-void CheckBuffer(const qsizetype aWordCount)
+void checkBuffer(const qsizetype aWordCount)
 {
     // Adjust buffer low and fill if necessary
     
     // Load buffer if necessary
 }
+
+QVector<unsigned> takeBuffer()
+{
+            
